9f9ee55e2e44d749077b0a3c3d5516ceee2aab4d
stxss <stasslearning@gmail.com>
2022-11-27 21:57:22 +0000

    Sun, 27 Nov , 2022, 21:57:22  +00:00

--- app.py
+++ app.py
@@ -78,6 +78,9 @@ async def filter_audio(client, message):
     )
     await message.reply("Please choose what you want to do with the file", reply_markup=choices)
 
+    
+
+
     print(message.voice, message.audio)
     audiofile = await message.download()
     sound = open(audiofile, "rb")
