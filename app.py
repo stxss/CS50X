07705b01b9cd062194f6b8176f82efaf99033281
stxss <stasslearning@gmail.com>
2022-11-27 22:29:59 +0000

    Sun, 27 Nov , 2022, 22:29:59  +00:00

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ async def filter_audio(client, message):
     await message.reply("Please choose what you want to do with the file", reply_markup=choices)
 
 @app.on_callback_query()
-async def transcription(client, ):
+async def transcription(client, )
 
 
 @...
