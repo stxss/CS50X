5758b1db7ea6df25506168f4d7942eeffcaae0f6
stxss <stasslearning@gmail.com>
2022-11-28 12:36:31 +0000

    Mon, 28 Nov , 2022, 12:36:31  +00:00

--- app.py
+++ app.py
@@ -81,42 +81,67 @@ async def filter_audio(client, message):
 
     await message.reply("Please choose what you want to do with the file", reply_markup=choices)  
 
-    audiofile = await message.download()
-    sound = open(audiofile, "rb")
-
-    if message.audio:
-        mimetype = "audio/mpeg"
-    elif message.voice:
-        mimetype = "audio/ogg"
-
-    source = {
-        "buffer": sound,
-        "mimetype": mimetype
-    }        
-
-    response = await asyncio.create_task(
-        deepgram.transcription.prerecorded(
-            source,
-            {
-                "punctuate": True 
-            }
-        )
-    )
-
-    reply = response["results"]["channels"][0]["alternatives"][0]["transcript"]
-
-    #await message.reply(reply)
-
+#    audiofile = await message.download()
+#    sound = open(audiofile, "rb")
+#
+#    if message.audio:
+#        mimetype = "audio/mpeg"
+#    elif message.voice:
+#        mimetype = "audio/ogg"
+#
+#    source = {
+#        "buffer": sound,
+#        "mimetype": mimetype
+#    }        
+#
+#    response = await asyncio.create_task(
+#        deepgram.transcription.prerecorded(
+#            source,
+#            {
+#                "punctuate": True 
+#            }
+#        )
+#    )
+#
+#    reply = response["results"]["channels"][0]["alternatives"][0]["transcript"]
+#
+#    #await message.reply(reply)
+#
     dir = config.folder_path
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
 
 
 @app.on_callback_query()
-async def choices_first(client, callback: CallbackQuery):
+async def choices_first(message=, callback: CallbackQuery):
     if callback.data == "transcribe":
+        
+        audiofile = await callback.message.download()
+        sound = open(audiofile, "rb")
+
+        if callback.message.audio:
+            mimetype = "audio/mpeg"
+        elif callback.message.voice:
+            mimetype = "audio/ogg"
+
+        source = {
+            "buffer": sound,
+            "mimetype": mimetype
+        }        
+
+        response = await asyncio.create_task(
+            deepgram.transcription.prerecorded(
+                source,
+                {
+                    "punctuate": True 
+                }
+            )
+        )
+
+        reply = response["results"]["channels"][0]["alternatives"][0]["transcript"]
+
         await callback.message.reply(reply)
-        await callback.message.reply("transcribe")
+        #await callback.message.reply("transcribe")
     elif callback.data == "trim":
         await callback.message.reply("trim")
 
