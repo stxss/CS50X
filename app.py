7d7b765e8b422b73aa46b308f4bc797db9fd6579
stxss <stasslearning@gmail.com>
2022-11-29 16:09:30 +0000

    Tue, 29 Nov , 2022, 16:09:30  +00:00

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ async def filter_audio(client, message):
     list_range = len(response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"])
     print(list_range)
     for i in range(0, list_range + 1):
-        for j in range(0, list_range + 1):
+        for j in range(0, list_range + 2):
             try:
                 print(i,j)
                 start_time = response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][i]["sentences"][j]["start"]
