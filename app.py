3d43fc5caaddb6358861ea739bbcb2a7b22ba63f
stxss <stasslearning@gmail.com>
2022-11-29 12:59:25 +0000

    Tue, 29 Nov , 2022, 12:59:25  +00:00

--- app.py
+++ app.py
@@ -112,9 +112,9 @@ async def filter_audio(client, message):
     #    reply_w_timestamp += response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][0]["sentences"][i]["text"]
     #    reply_w_timestamp += "\n\n"
     for i in range(len(response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][0]["sentences"])):
-        reply_w_timestamp += str(response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][i]["sentences"][i]["start"]) + " to "
-        reply_w_timestamp += str(response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][i]["sentences"][i]["end"]) + "\n" 
-        reply_w_timestamp += response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][i]["sentences"][i]["text"]
+        reply_w_timestamp += str(response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][0]["sentences"][i]["start"]) + " to "
+        reply_w_timestamp += str(response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][0]["sentences"][i]["end"]) + "\n" 
+        reply_w_timestamp += response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"][0]["sentences"][i]["text"]
         reply_w_timestamp += "\n\n"
 
 
