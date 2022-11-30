563132b08a5b478a4942242dbe027ceaa573df17
stxss <stasslearning@gmail.com>
2022-11-30 21:31:53 +0000

    Wed, 30 Nov , 2022, 21:31:53  +00:00

--- helpers.py
+++ helpers.py
@@ -3,10 +3,13 @@ import re
 async def trim_voice(message):
     pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
     check = message.text
-
+    reply_if_fail = ""
     while True:
+        await message.reply(reply_if_fail)
         if not pattern.match(check):
-            await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
+            reply_if_fail = "Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40"
+            #await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
+            await message.reply(reply_if_fail)
             continue
         else:
             await message.reply(message.text)
