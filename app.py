796c5950f00b84adaf5df42a9377cbc330734520
stxss <stasslearning@gmail.com>
2022-11-30 19:05:14 +0000

    Wed, 30 Nov , 2022, 19:05:14  +00:00

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ async def invalid_file(client, message):
 
 
 
-@app.on_callback_query()
+@app.on_callback_query(filters.private)
 async def choice_trim(message, callback: CallbackQuery):
     if callback.data == "trim_audio":
         await callback.message.reply("Please send the times of the desired trim in (mm:ss). (e.g. 00:13-01:40)")
