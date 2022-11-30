0fa7471b24e51807007c1c93c9af196c376059e0
stxss <stasslearning@gmail.com>
2022-11-30 21:20:44 +0000

    Wed, 30 Nov , 2022, 21:20:44  +00:00

--- helpers.py
+++ helpers.py
@@ -1,8 +1,13 @@
 import re
+import app
 
-async def trim(message):
+
+async def trim():
+    message = await app.ask(text="Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40",chat_id=chat_id.chat_id, timeout=30)
+
     pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
     check = message.text
+
     while True:
         if not pattern.match(check):
             await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
