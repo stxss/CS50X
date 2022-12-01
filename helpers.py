670acab2d886d138cb0221c319b485fda550da7b
stxss <stasslearning@gmail.com>
2022-12-01 16:28:33 +0000

    Thu, 1 Dec , 2022, 16:28:33  +00:00

--- helpers.py
+++ helpers.py
@@ -1,8 +1,9 @@
 import re
 import asyncio
-
+import ffmpeg
 import config
 import os
+import datetime
 
 path = config.path
 
@@ -10,8 +11,11 @@ async def trim_voice(message):
     pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
     check = message.text
     reply_if_fail = ""
-    if pattern.match(check):
-        await message.reply(message.text)
+    if pattern.match(check):        
+        probe_res = ffmpeg.probe("downloads\\voicefile.ogg")
+        duration = probe_res.get("format", {}).get("duration", None)
+
+        await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-4])
     else:
         reply_if_fail = "Invalid range\n\nPlease resend the audio (or forward it again to me) and when selecting the trim option, input a valid range of the times of the desired trim in [mm:ss - mm:ss].\n\nFor example: 00:13-01:40"
         await message.reply(reply_if_fail)
--- test.py
+++ test.py
@@ -1,76 +1,10 @@
 import os
 import config
 import datetime
-#from __future__ import unicode_literals
-#import youtube_dl
-#
-#import json
-#import requests
-#import urllib.request
-#
-#
-#
-##f = open("help.txt", "r")
-##print(f.read())
-#
-#
-#url_yt = "https://www.youtube.com/watch?v=3oFAJtFE8YU"
-#
-#def get_url(url):
-#    response = requests.get(url)
-#    content = response.content.decode("utf8")
-#    return content
-#
-#def get_json_from_url(url):
-#    content = get_url(url)
-#    js = json.loads(content)
-#    return js
-#
-#
-#class MyLogger(object):
-#    def debug(self, msg):
-#        pass
-#
-#    def warning(self, msg):
-#        pass
-#
-#    def error(self, msg):
-#        print(msg)
-#
-#
-#def my_hook(d):
-#    if d['status'] == 'finished':
-#        print('Done downloading, now converting ...')
-#
-#
-##ydl_opts = {
-##    'format': 'bestaudio/best',
-##    'postprocessors': [{
-##        'key': 'FFmpegExtractAudio',
-##        'preferredcodec': 'mp4',
-##        'preferredquality': '192',
-##    }],
-##    'logger': MyLogger(),
-##    'progress_hooks': [my_hook],
-##}
-#ydl_opts= {}
-#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#    ydl.download(['https://www.youtube.com/watch?v=3oFAJtFE8YU'])
-import os
 import ffmpeg
 
 
 probe_res = ffmpeg.probe("downloads\\voicefile.ogg")
 duration = probe_res.get("format", {}).get("duration", None)
-
-#print(type(duration))
-print(float(duration) // 1)
-print(float(duration) % 1)
-seconds = float(duration) // 1
-ms = float(duration) % 1
-
-seconds, ms = divmod(ms, 1000)
-minutes, seconds = divmod(seconds, 60)
-print(f'{int(minutes):01d}:{int(seconds):02d}')
 
 print(str(datetime.timedelta(seconds=float(duration)))[:-4])
