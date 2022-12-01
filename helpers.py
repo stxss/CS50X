62cb6ff28ed0dc949f5a88f93cbb1d750057507b
stxss <stasslearning@gmail.com>
2022-12-01 18:31:08 +0000

    Thu, 1 Dec , 2022, 18:31:08  +00:00

--- helpers.py
+++ helpers.py
@@ -62,7 +62,7 @@ async def trim_voice(message, filetype):
 
 
 
-        await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-7])
+        #await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-7])
     else:
         reply_if_fail = "Invalid range\n\nPlease resend the audio (or forward it again to me) and when selecting the trim option, input a valid range of the times of the desired trim in [mm:ss - mm:ss].\n\nFor example: 00:13-01:40"
         await message.reply(reply_if_fail)
