1d92902501e81acbbb98bf18b4a31d473e06878e
stxss <stasslearning@gmail.com>
2022-12-01 12:15:14 +0000

    Thu, 1 Dec , 2022, 12:15:14  +00:00

--- helpers.py
+++ helpers.py
@@ -1,22 +1,35 @@
 import re
 import asyncio
 
-async def trim_voice(message):
-    pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
-    check = message.text
-    reply_if_fail = ""
-    while True:
-        if pattern.match(check):
-            await message.reply(message.text)
-            break 
-        else:
-            reply_if_fail = "Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40"
-            #await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
-            await message.reply(reply_if_fail)
+#async def trim_voice(message):
+#    pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
+#    check = message.text
+#    reply_if_fail = ""
+#    while True:
+#        if pattern.match(check):
+#            await message.reply(message.text)
+#            break 
+#        else:
+#            reply_if_fail = "Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40"
+#            #await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
+#            await message.reply(reply_if_fail)
+#
+#            continue
 
-            continue
-
-            
+#async def trim_voice(message):
+#    pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
+#    check = message.text
+#    reply_if_fail = ""
+#    while True:
+#        if pattern.match(check):
+#            await message.reply(message.text)
+#            break 
+#        else:
+#            reply_if_fail = "Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40"
+#            #await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
+#            await message.reply(reply_if_fail)
+#
+#            continue
 
     
     
