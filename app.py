5a3670215afe814d977c8ed423ba2d04f2527d0e
stxss <stasslearning@gmail.com>
2022-12-01 18:43:34 +0000

    Thu, 1 Dec , 2022, 18:43:34  +00:00

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ async def choice_from_inline(message, callback: CallbackQuery):
         except asyncio.exceptions.TimeoutError:
             await callback.message.reply("Something went wrong, please try again")
         
-        #await app.send_audio(chat_id=chat_id, audio="downloads\out.mp3")
+        await app.send_audio(chat_id=chat_id, audio="downloads\out.mp3")
         await callback.message.reply_audio(audio="downloads\out.mp3")
     elif callback.data == "transcribe":
         with open(os.path.join(config.path, "transcription.txt"), "r", encoding="utf-8") as f1:
