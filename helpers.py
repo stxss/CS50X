9e560065d200b01e6fb93989c15ca5dddcee85f6
stxss <stasslearning@gmail.com>
2022-12-01 19:09:46 +0000

    Thu, 1 Dec , 2022, 19:09:46  +00:00

--- helpers.py
+++ helpers.py
@@ -55,7 +55,7 @@ async def trim_voice(message, filetype):
         input_stream = ffmpeg.input(in_file)
         pts = "PTS-STARTPTS"
         file_trim = (input_stream.filter_("atrim", start=start_trim_time, end=end_trim_time).filter_("asetpts", pts))
-        output = ffmpeg.output(file_trim, "downloads\\out.mp3" , format = "mp3")
+        output = ffmpeg.output(file_trim, "downloads\out.mp3" , format = "mp3")
         output.run()
 
         await message.reply_audio(message.chat.id, "downloads\\out.mp3")
