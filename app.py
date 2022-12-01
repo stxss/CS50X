0224bc3258c286066850d0962808624a3810db39
stxss <stasslearning@gmail.com>
2022-12-01 12:48:44 +0000

    Thu, 1 Dec , 2022, 12:48:44  +00:00

--- app.py
+++ app.py
@@ -243,8 +243,7 @@ async def choice_from_inline(message, callback: CallbackQuery):
                     reply_if_fail = "Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40"
                     #await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
                     await callback.message.reply(reply_if_fail)
-                    asyncio.sleep(10)
-                    #continue
+                    break
             
             await helpers.trim_voice(trim_length)
             # could implement the regex testing here and if the user doesn't answer correctly, 
--- helpers.py
+++ helpers.py
@@ -1,20 +1,8 @@
 import re
 import asyncio
 
-#async def trim_voice(message):
-#    pattern = re.compile("^(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))-(([0]?[0-5][0-9]|[0-9]):([0-5][0-9]))$")
-#    check = message.text
-#    reply_if_fail = ""
-#    while True:
-#        if pattern.match(check):
-#            await message.reply(message.text)
-#            break 
-#        else:
-#            reply_if_fail = "Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40"
-#            #await message.reply("Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40")
-#            await message.reply(reply_if_fail)
-#
-#            continue
+async def trim_voice(message):
+    ...
 
                   
 
