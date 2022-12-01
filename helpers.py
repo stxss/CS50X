ede98bb2c0e339c00c239cd45cfa0c0302ecb760
stxss <stasslearning@gmail.com>
2022-12-01 17:23:30 +0000

    Thu, 1 Dec , 2022, 17:23:30  +00:00

--- helpers.py
+++ helpers.py
@@ -17,15 +17,15 @@ async def trim_voice(message):
         
         user_duration = check.split("-")
         
-        start_time = user_duration[0]
-        user_start_mins = int(start_time.split(":")[0])
-        user_start_sec = int(start_time.split(":")[1])
-        #print(start_time, user_start_mins, user_start_sec)
+        user_start_time = user_duration[0]
+        user_start_mins = int(user_start_time.split(":")[0])
+        user_start_sec = int(user_start_time.split(":")[1])
+        
 
-        end_time = user_duration[1]
-        user_end_mins = int(end_time.split(":")[0])
-        user_end_sec = int(end_time.split(":")[1])
-        #print(end_time, user_end_mins, user_end_sec)
+        user_end_time = user_duration[1]
+        user_end_mins = int(user_end_time.split(":")[0])
+        user_end_sec = int(user_end_time.split(":")[1])
+        
 
         file_start_time = "00:00"
         file_end_time = str(datetime.timedelta(seconds=float(duration)))[:-7]
@@ -37,7 +37,15 @@ async def trim_voice(message):
         # splitting with 1 and 2 because the file time output is in h:mm:ss
         file_end_time_mins = file_end_time.split(":")[1] 
         file_end_time_sec = file_end_time.split(":")[2] 
-        print(file_end_time_mins, file_end_time_sec)
+        
+        if user_start_time < file_start_time:
+            ...
+        elif user_end_time > file_end_time: 
+            ...
+
+
+
+            
         await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-7])
     else:
         reply_if_fail = "Invalid range\n\nPlease resend the audio (or forward it again to me) and when selecting the trim option, input a valid range of the times of the desired trim in [mm:ss - mm:ss].\n\nFor example: 00:13-01:40"
