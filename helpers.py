c116a77de719395cf84eee58866f5cb9c0d90236
stxss <stasslearning@gmail.com>
2022-12-01 16:41:07 +0000

    Thu, 1 Dec , 2022, 16:41:07  +00:00

--- helpers.py
+++ helpers.py
@@ -14,12 +14,26 @@ async def trim_voice(message):
     if pattern.match(check):        
         probe_res = ffmpeg.probe("downloads\\voicefile.ogg")
         duration = probe_res.get("format", {}).get("duration", None)
-        user_duration = check.split("-")
-        start_time = user_duration[0]
-        end_time = user_duration[1]
         
-        print(user_duration)
-        await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-4])
+        user_duration = check.split("-")
+        
+        start_time = user_duration[0]
+        user_start_mins = start_time.split(":")[0]
+        user_start_sec = start_time.split(":")[1]
+        print(start_time, user_start_mins, user_start_sec)
+
+        end_time = user_duration[1]
+        user_end_mins = end_time.split(":")[0]
+        user_end_sec = end_time.split(":")[1]
+        print(end_time, user_end_mins, user_end_sec)
+
+        file_start_time = "0:00:00"
+        file_end_time = str(datetime.timedelta(seconds=float(duration)))[:-7]
+
+        #file_start_time_actual = file_start_time[...] 
+        #file_end_time_actual = file_end_time[...] 
+        
+        await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-7])
     else:
         reply_if_fail = "Invalid range\n\nPlease resend the audio (or forward it again to me) and when selecting the trim option, input a valid range of the times of the desired trim in [mm:ss - mm:ss].\n\nFor example: 00:13-01:40"
         await message.reply(reply_if_fail)
