46887db5a3576b42bb81964219b646ab62b13d9c
stxss <stasslearning@gmail.com>
2022-12-01 17:19:28 +0000

    Thu, 1 Dec , 2022, 17:19:28  +00:00

--- helpers.py
+++ helpers.py
@@ -27,12 +27,17 @@ async def trim_voice(message):
         user_end_sec = int(end_time.split(":")[1])
         #print(end_time, user_end_mins, user_end_sec)
 
-        file_start_time = "0:00:00"
+        file_start_time = "00:00"
         file_end_time = str(datetime.timedelta(seconds=float(duration)))[:-7]
 
-        file_start_time_actual = file_start_time[...] 
-        file_end_time_actual = file_end_time[...] 
+        # Splitting in 0 and 1 because the file_start_time that i wrote is in mm:ss
+        file_start_time_mins = file_start_time.split(":")[0] 
+        file_start_time_sec = file_start_time.split(":")[1]
         
+        # splitting with 1 and 2 because the file time output is in h:mm:ss
+        file_end_time_mins = file_end_time.split(":")[1] 
+        file_end_time_sec = file_end_time.split(":")[2] 
+        print(file_end_time_mins, file_end_time_sec)
         await message.reply(str(datetime.timedelta(seconds=float(duration)))[:-7])
     else:
         reply_if_fail = "Invalid range\n\nPlease resend the audio (or forward it again to me) and when selecting the trim option, input a valid range of the times of the desired trim in [mm:ss - mm:ss].\n\nFor example: 00:13-01:40"
