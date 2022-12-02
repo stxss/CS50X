c2cd5fe0f3963fef3311d31e6747ff9fd8274f20
stxss <stasslearning@gmail.com>
2022-12-02 19:37:40 +0000

    Fri, 2 Dec , 2022, 19:37:40  +00:00

--- app.py
+++ app.py
@@ -263,18 +263,18 @@ async def choice_from_inline(Client, callback: CallbackQuery):
         except asyncio.exceptions.TimeoutError:
             await callback.message.reply("Something went wrong, please try again")
 
-    elif callback.data == "trim_voice":
-        try:
-            trim_length = await app.ask(
-                text="Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40",
-                chat_id=chat_id.chat_id,
-                timeout=30,
-            )
-            await helpers.trim_voice(trim_length, "voice")
-          
-            await app.send_audio(
-                chat_id=chat_id.chat_id, audio=os.path.join(config.path, "out.mp3")
-            )
+    #elif callback.data == "trim_voice":
+    #    try:
+    #        trim_length = await app.ask(
+    #            text="Please send the times of the desired trim in [mm:ss - mm:ss].\nFor example: 00:13-01:40",
+    #            chat_id=chat_id.chat_id,
+    #            timeout=30,
+    #        )
+    #        await helpers.trim_voice(trim_length, "voice")
+    #      
+    #        await app.send_audio(
+    #            chat_id=chat_id.chat_id, audio=os.path.join(config.path, "out.mp3")
+    #        )
 
         except asyncio.exceptions.TimeoutError:
             await callback.message.reply("Something went wrong, please try again")
