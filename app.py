7bfed484fd43141321ca0490a957ec52bf0169d6
stxss <stasslearning@gmail.com>
2022-12-03 15:07:32 +0000

    Sat, 3 Dec , 2022, 15:07:32  +00:00

--- app.py
+++ app.py
@@ -207,9 +207,9 @@ async def filter_audio(client, message):
         choices = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Transcribe", callback_data="transcribe", user_id=chat_id),
+                    InlineKeyboardButton("Transcribe", callback_data="transcribe"),
                     InlineKeyboardButton(
-                        "Transcribe w/ timestamps", callback_data="timestamp"
+                        "Transcribe w/ timestamps", callback_data="timestamp",
                     )
                 ],
                 [
@@ -256,8 +256,8 @@ async def choice_from_inline(Client, callback: CallbackQuery):
     #                chat_id_for_join = line[9:]
     #            if line.startswith("sent_img"):
     #                sent_img_val = line[10:]
-    print(callback.)
-    with open(f"downloads\\{chat_id}\chat_id.py", "r", encoding="utf-8") as f:
+    #print(callback.from_user.id)
+    with open(f"downloads\\{callback.from_user.id}\chat_id.py", "r", encoding="utf-8") as f:
             for line in f:
                 if line.startswith("chat_id"):
                     chat_id_for_join = line[9:]
