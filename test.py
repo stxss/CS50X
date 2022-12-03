4d0c8f8c91a8d542fa761f56e2bbc3d099510baf
stxss <stasslearning@gmail.com>
2022-12-03 15:50:50 +0000

    Sat, 3 Dec , 2022, 15:50:50  +00:00

--- test.py
+++ test.py
@@ -4,14 +4,15 @@ import datetime
 import ffmpeg
 
 
-probe_res = ffmpeg.probe("downloads\\voicefile.ogg")
-duration = probe_res.get("format", {}).get("duration", None)
 
-print(str(datetime.timedelta(seconds=float(duration)))[:-4])
+#probe_res = ffmpeg.probe("downloads\\voicefile.ogg")
+#duration = probe_res.get("format", {}).get("duration", None)
+
+#print(str(datetime.timedelta(seconds=float(duration)))[:-4])
 
 
-probe = ffmpeg.probe(f"downloads\\{971531412}\\imagefile.jpg")
-#width = int(probe['width'])
-#height = int(probe['height'])
+probe = ffmpeg.probe(f"downloads\971531412\imagefile.jpg")
+width = int(probe['width'])
+height = int(probe['height'])
 
-print(probe)
+print(probe, width, height)
