a11a0f85e2b5bda5e7a1928c2ab98780af24dc3a
stxss <stasslearning@gmail.com>
2022-12-05 22:10:24 +0000

    Mon, 5 Dec , 2022, 22:10:24  +00:00

--- helpers.py
+++ helpers.py
@@ -1,14 +1,11 @@
 import re
 import asyncio
 import ffmpeg
-#import config
+import config
 import os
 import datetime
-from os import getenv, listdir, remove
-from dotenv import load_dotenv
 
-#path = config.path
-path = getenv("path")
+path = config.path
 
 # Choices for joining
 
