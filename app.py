246c104a0384703f7b53170c910724305510971a
stxss <stasslearning@gmail.com>
2022-12-06 18:16:04 +0000

    Tue, 6 Dec , 2022, 18:16:04  +00:00

--- app.py
+++ app.py
@@ -338,10 +338,10 @@ async def choice_from_inline(Client, callback: CallbackQuery):
 
 now = time.time()
 dir = "./downloads"
-async def file_age_check():
-    for f in os.listdir(dir):
-        if os.stat(os.path.join(f"downloads", f)).st_mtime < now - 30:
-           os.remove(os.path.join(dir, f))  
+
+for f in os.listdir(dir):
+    if os.stat(os.path.join(f"downloads", f)).st_mtime < now - 30:
+        os.remove(os.path.join(dir, f))
 
 
 if __name__ == "__main__":
